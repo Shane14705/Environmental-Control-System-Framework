c80f5df8b8fa8a8349306d8de256ea3540005a03
Shane McKelvey <psmnhb@umsystem.edu>
2022-08-23 16:48:27 -0500

    began writing dht timer callback

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <utils.h>
-
+#include <perceiver.h>
+#include <vector>
+#include <DHT.h>
 #define NUMINPUTS 2
 
 
@@ -8,7 +10,12 @@ void setup() {
   //setup our input pins
   int INPUTPINS[NUMINPUTS] = {34, 35};
   setupInputs(INPUTPINS, NUMINPUTS);
-  
+  for (int i = 0; i < NUMINPUTS; i++) {
+    //sensors.insert(sensors.cbegin(), );
+    
+  }
+  //Perceiver perception(sensors);
+
 }
 
 void loop() {
--- src/perceiver.cpp
+++ src/perceiver.cpp
@@ -4,14 +4,19 @@
 //Sensor delay in milliseconds. Eventually this will be passed in as part of a struct that allows for user defined sensors.
 #define SENSOR_DELAYMS 5000
 
+//This task should simply poll the given sensor, and send a task notification the controller task with the new readings
+void xDHTTimerCallback(TimerHandle_t handle) {
+
+};
 
 Perceiver::Perceiver(std::vector<DHT> dhtContainer) {
     //Constructor: what is this class's invariant? What should always be true about it?
 
 
-    for (DHT sensor : dhtContainer) {
-        //xTimerCreate(NULL, (SENSOR_DELAYMS / portTICK_RATE_MS), pdTRUE, )
-        //Need to figure out how the timer task should share its polled values with the other tasks.
+    for (int i = 0; i < dhtContainer.size(); i++) {
+        dhtContainer[i].begin();
+        TimerHandles.insert(TimerHandles.cbegin(), xTimerCreate(NULL, (SENSOR_DELAYMS / portTICK_RATE_MS), pdTRUE, NULL, &xDHTTimerCallback));
+        
     }
 
 };

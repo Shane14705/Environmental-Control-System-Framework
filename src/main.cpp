d667239b135936df95be74fb3b2a16eb8a7a05f9
Shane McKelvey <psmnhb@umsystem.edu>
2022-08-23 22:59:49 -0500

    set up DHT sensor array

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,26 @@
 #include <utils.h>
-#include <perceiver.h>
+//#include <perceiver.h>
 #include <vector>
+#include <memory>
 #include <DHT.h>
 #define NUMINPUTS 2
 
+void xDHTTimerCallback();
 
+std::vector<std::unique_ptr<DHT>> SENSOR_ARRAY;
 
 void setup() {
   //setup our input pins
   int INPUTPINS[NUMINPUTS] = {34, 35};
   setupInputs(INPUTPINS, NUMINPUTS);
+
   for (int i = 0; i < NUMINPUTS; i++) {
-    //sensors.insert(sensors.cbegin(), );
+    //delete ptr is just the same as ptr->~Type();
+    //very different call from ~ptr();
+    SENSOR_ARRAY.push_back(std::unique_ptr<DHT>(new DHT(INPUTPINS[i], DHT22)));
     
   }
+
   //Perceiver perception(sensors);
 
 }
